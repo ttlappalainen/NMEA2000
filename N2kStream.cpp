bd6b521bda6b1766c174dc818311d730c52dacd6
Marco Nilsson <marco@ormgas.com>
2016-12-07 12:15:51 +0100

    Fix N2kStream for AVR targets that aren't Arduinos.
    Also fix incorrect itoa usage.

--- N2kStream.cpp
+++ N2kStream.cpp
@@ -12,9 +12,21 @@ size_t N2kStream::print(const char *str) {
    return write(reinterpret_cast<const uint8_t*>(str), strlen(str));
 }
 
+#if defined(__AVR__)
+size_t N2kStream::print(const __FlashStringHelper* str) {
+   size_t bytes_written = 0;
+   PGM_P src = (PGM_P) str;
+   for(uint8_t c = pgm_read_byte(src); c != 0; ++src, ++bytes_written)
+      write(&c, 1);
+
+   return bytes_written;
+}
+#endif
+
+
 size_t N2kStream::print(int val) {
    char buf[16];
-   return write(itoa(buf, val, 10), strlen(buf));
+   return write((const uint8_t*) itoa(val, buf, 10), strlen(buf));
 }
 
 size_t N2kStream::println(const char *str) {
