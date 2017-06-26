5b2876ae33984d9a0dc517e36fd45bf7c331dc0d
ttlappalainen <timo.lappalainen@kave.fi>
2017-06-26 22:25:02 +0300

    Example updates

--- Examples/ActisenseListenerSender/ActisenseListenerSender.ino
+++ Examples/ActisenseListenerSender/ActisenseListenerSender.ino
@@ -15,14 +15,25 @@
 
 tActisenseReader ActisenseReader;
 
+// Define READ_STREAM to port, where you write data from PC e.g. with NMEA Simulator.
+#define READ_STREAM Serial       
+// Define ForwardStream to port, what you listen on PC side. On Arduino Due you can use e.g. SerialUSB
+#define FORWARD_STREAM Serial    
+
+Stream *ReadStream=&READ_STREAM;
+Stream *ForwardStream=&FORWARD_STREAM;
 
 void setup() {
   // Define buffers big enough
   NMEA2000.SetN2kCANSendFrameBufSize(150);
   NMEA2000.SetN2kCANReceiveFrameBufSize(150);
   
+  if (ReadStream!=ForwardStream) READ_STREAM.begin(115200);
+  FORWARD_STREAM.begin(115200);
+  NMEA2000.SetForwardStream(ForwardStream); 
   NMEA2000.SetMode(tNMEA2000::N2km_ListenAndSend);
   // NMEA2000.SetForwardType(tNMEA2000::fwdt_Text); // Show bus data in clear text
+  if (ReadStream==ForwardStream) NMEA2000.SetForwardOwnMessages(false); // If streams are same, do not echo own messages.
   // NMEA2000.EnableForward(false);
   NMEA2000.Open();
 
