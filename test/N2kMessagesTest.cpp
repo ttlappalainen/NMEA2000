db86178758fa4eb4a41bd879b592c58797fba57d
Matthew Cairns <matthew@openoceanrobotics.com>
2022-02-07 13:26:30 -0800

    Add tN2kDD305 for PGN 129041, resolve issue with GNSSType

--- test/N2kMessagesTest.cpp
+++ test/N2kMessagesTest.cpp
@@ -162,7 +162,6 @@ TEST_CASE("PGN129039 AIS Class B Position")
 
 TEST_CASE("PGN129041 AIS AtoN Navigation Report") 
 {
-
   tN2kMsg N2kMsg;
   tN2kAISAtoNReportData data_tx;
 
@@ -186,15 +185,12 @@ TEST_CASE("PGN129041 AIS AtoN Navigation Report")
   data_tx.AtoNStatus = 0x00;
   data_tx.AISTransceiverInformation =  N2kaischannel_B_VDL_transmission;
   data_tx.SetAtoNName("BINGBONG");
-  size_t AtoNNameMaxSize = 34;
-
 
   SetN2kAISAtoNReport(N2kMsg, data_tx);
 
-
   char AtoNName_RX[34];
   tN2kAISAtoNReportData data_rx;
-  ParseN2kAISAtoNReport(N2kMsg, data_rx, AtoNNameMaxSize);
+  ParseN2kAISAtoNReport(N2kMsg, data_rx);
 
   SECTION("parsed values match set values")
   {
